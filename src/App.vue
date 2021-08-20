c2312da28e30774bd7582b73d6aa7c482c61def7
杨光 <yangguang6@corp.netease.com>
2021-08-20 17:56:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <p id="app1">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+}
+</script>
+
+<style>
+#app1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
